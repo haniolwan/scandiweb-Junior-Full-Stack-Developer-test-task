91224bd6a9636757010b531127a4a139b857159a
haniolwan <heenoow@gmail.com>
2025-05-21 04:28:18 +0300

    edit env variables

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -11,15 +11,12 @@ class Database
     protected function connect()
     {
         try {
-            $dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
-            $dotenv->load();
-
             // Should be hidden for testing purposes only
-            $host = $_ENV['DB_HOST'] ?? 'localhost';
-            $dbname = $_ENV['DB_NAME'] ?? 'scandiweb';
-            $username = $_ENV['DB_USER'] ?? 'root';
-            $password = $_ENV['DB_PASSWORD'] ?? 'StrongP@ssw0rd!';
-            $port = $_ENV['DB_PORT'] ?? '3306';
+            $host = 'localhost';
+            $dbname = 'scandiweb';
+            $username = 'root';
+            $password = 'StrongP@ssw0rd!';
+            $port = '3306';
 
             $pdo = new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $username, $password);
             return $pdo;
